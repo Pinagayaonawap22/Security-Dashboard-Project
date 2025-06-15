e5e15a2133a6484234b77c03f9d21fda2dbff946
Pinagayaonawap22 <pinagayaonawap22@gmail.com>
2025-06-15 12:41:06 +0800

    Initial commit: Flask Cybersecurity Dashboard

--- db/db_config.py
+++ db/db_config.py
@@ -4,8 +4,8 @@ import mysql.connector
 def get_connection():
     return mysql.connector.connect(
         host=os.getenv("MYSQLHOST"),
-        port=int(os.getenv("MYSQLPORT")),  # 👈 must convert to int
         user=os.getenv("MYSQLUSER"),
-        password=os.getenv("MYSQLPASSWORD"),
-        database=os.getenv("MYSQLDATABASE")
+        password=os.getenv("MYSQLPASS"),
+        database=os.getenv("MYSQLDB"),
+        port=int(os.getenv("MYSQLPORT", 3306))
     )
