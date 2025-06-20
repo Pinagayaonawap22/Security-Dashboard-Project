60d213475badde26b2eecf40aee2281820c1e31a
Pinagayaonawap22 <pinagayaonawap22@gmail.com>
2025-06-20 08:51:05 +0800

    Add Procfile for Railway start command

--- app.py
+++ app.py
@@ -19,6 +19,6 @@ conn.close()
 init_routes(app)
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port)
+    import os
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
 
