9c832493bb3fb1da3df19590ac88ed0955fe26e8
Pinagayaonawap22 <pinagayaonawap22@gmail.com>
2025-06-20 09:13:42 +0800

    Add Procfile for Railway start command

--- db/db_config.py
+++ db/db_config.py
@@ -6,10 +6,9 @@ load_dotenv()
 
 def get_connection():
     return mysql.connector.connect(
-        host="containers-us-west-17.railway.app",
-        user="root",
-        password="zSZTEQIkTIvckSAbxaqiIMqphRyuUfzs",
-        database="railway",
-        port=17843,
-        unix_socket=None
+        host=os.getenv("MYSQLHOST", "mysql-xl07.railway.internal"),
+        user=os.getenv("MYSQLUSER", "root"),
+        password=os.getenv("MYSQLPASSWORD", "zSZTEQIkTIvckSAbxaqiIMqphRyuUfzs"),
+        database=os.getenv("MYSQLDATABASE", "railway"),
+        port=int(os.getenv("MYSQLPORT", 3306))
     )
