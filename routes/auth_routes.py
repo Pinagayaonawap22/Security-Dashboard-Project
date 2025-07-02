462ba3f1f0291bedc44b26f053cece41abc638ef
Pinagayaonawap22 <pinagayaonawap22@gmail.com>
2025-07-02 19:29:23 +0800

    Add Procfile for Railway start command

--- routes/auth_routes.py
+++ routes/auth_routes.py
@@ -11,7 +11,7 @@ bp = Blueprint('auth_routes', __name__)
 def home():
     return redirect(url_for('auth_routes.register'))
 
-@bp.route('/register', methods=['GET', 'POST'])
+@bp.route('/register', methods=['GET', 'POST'], endpoint='register_user')
 def register():
     if request.method == 'POST':
         username = request.form.get('username')
