547cf7817f81961d7f3caa92e70fe458d9a837fd
Pinagayaonawap22 <pinagayaonawap22@gmail.com>
2025-07-03 08:11:24 +0800

    Add Procfile for Railway start command

--- db/db_config.py
+++ db/db_config.py
@@ -1,23 +1,23 @@
+from dotenv import load_dotenv
+load_dotenv()
+
 import os
 import mysql.connector
-from dotenv import load_dotenv
-
-load_dotenv()
 
 def get_connection():
     host = os.getenv("DB_HOST")
+    port = os.getenv("DB_PORT")
     user = os.getenv("DB_USER")
     password = os.getenv("DB_PASSWORD")
     database = os.getenv("DB_NAME")
-    port = os.getenv("DB_PORT")
 
-    if None in [host, user, password, database, port]:
+    if not all([host, port, user, password, database]):
         raise ValueError("One or more DB environment variables are missing")
 
     return mysql.connector.connect(
         host=host,
+        port=int(port),
         user=user,
         password=password,
-        database=database,
-        port=int(port)
+        database=database
     )
